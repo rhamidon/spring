f89d60e6926fad28a061dcedae159236ff787061
rhamidon <dcsd143045@student.nibm.lk>
2018-11-28 11:14:24 +0530

    moved config.xml to a package & loaded container from that package

--- 01_springcore/src/main/java/com/riyaz/spring/_springcore/App.java
+++ 01_springcore/src/main/java/com/riyaz/spring/_springcore/App.java
@@ -1,13 +1,22 @@
 package com.riyaz.spring._springcore;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		// load the container
+		ClassPathXmlApplicationContext ctx = 
+				new ClassPathXmlApplicationContext("com/riyaz/spring/resources/config.xml");
+		
+		// get a bean
+		Employee emp = (Employee) ctx.getBean("emp");
+		System.out.println(emp.getId() + ": " + emp.getName());
+		
+		// close the container
+		ctx.close();
+	}
 }
