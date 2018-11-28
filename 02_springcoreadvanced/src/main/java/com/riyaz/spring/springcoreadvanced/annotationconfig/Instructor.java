88d680350b432a5f45804f134777598a93a2dfbb
rhamidon <dcsd143045@student.nibm.lk>
2018-11-28 18:11:28 +0530

    used @Value(#{name}) to assign Collection to data

--- 02_springcoreadvanced/src/main/java/com/riyaz/spring/springcoreadvanced/annotationconfig/Instructor.java
+++ 02_springcoreadvanced/src/main/java/com/riyaz/spring/springcoreadvanced/annotationconfig/Instructor.java
@@ -1,5 +1,7 @@
 package com.riyaz.spring.springcoreadvanced.annotationconfig;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -7,12 +9,15 @@ import org.springframework.stereotype.Component;
 @Component("inst")
 @Scope("prototype")
 public class Instructor {
-	
+
 	@Value("10")
 	private int id;
-	
+
 	@Value("Bharath Thippireddy")
 	private String name;
+
+	@Value("#{topics}")
+	List<String> topics;
 
 	public int getId() {
 		return id;
@@ -30,9 +35,17 @@ public class Instructor {
 		this.name = name;
 	}
 
+	public List<String> getTopics() {
+		return topics;
+	}
+
+	public void setTopics(List<String> topics) {
+		this.topics = topics;
+	}
+
 	@Override
 	public String toString() {
-		return "Instructor [id=" + id + ", name=" + name + "]";
+		return "Instructor [id=" + id + ", name=" + name + ", topics=" + topics + "]";
 	}
 
 }
