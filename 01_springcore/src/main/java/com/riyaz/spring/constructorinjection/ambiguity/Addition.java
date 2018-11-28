84d3c86c6fd94136e768b92f4a11c9e99e295193
rhamidon <dcsd143045@student.nibm.lk>
2018-11-28 15:38:45 +0530

    done construction inj ambiguity problem

--- 01_springcore/src/main/java/com/riyaz/spring/constructorinjection/ambiguity/Addition.java
+++ 01_springcore/src/main/java/com/riyaz/spring/constructorinjection/ambiguity/Addition.java
@@ -2,15 +2,8 @@ package com.riyaz.spring.constructorinjection.ambiguity;
 
 public class Addition {
 
-	public Addition(double a, double b) {
-		System.out.println(">> constructor double");
-	}
-	
-	public Addition(int a, int b) {
-		System.out.println(">> constructor int");
-	}
-	
-	public Addition(String a, String b) {
-		System.out.println(">> constructor String");
+	public Addition(int a, double b) {
+		System.out.println(">> constructor");
+		System.out.println(a + ": " + b);
 	}
 }
