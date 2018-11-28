ac98003d38aa4d7fe7b305333749535cbc967842
rhamidon <dcsd143045@student.nibm.lk>
2018-11-28 16:30:55 +0530

    autowiring deoendencies byType using config.xml

--- 02_springcoreadvanced/src/main/java/com/riyaz/spring/springcoreadvanced/autowiring/App.java
+++ 02_springcoreadvanced/src/main/java/com/riyaz/spring/springcoreadvanced/autowiring/App.java
@@ -1,13 +1,17 @@
 package com.riyaz.spring.springcoreadvanced.autowiring;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx = 
+				new ClassPathXmlApplicationContext("com/riyaz/spring/springcoreadvanced/autowiring/config.xml");
+		
+		Employee e = ctx.getBean("emp", Employee.class);
+		System.out.println(e);
+		ctx.close();
+	}
+
 }
