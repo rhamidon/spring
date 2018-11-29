b360238a1307bbe7166c09f650b994eb5dc5c982
rhamidon <dcsd143045@student.nibm.lk>
2018-11-29 11:21:20 +0530

    add Employee POJO, DAO and DAOImpl

---
+++ 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/employee/dao/EmployeeDAO.java
@@ -0,0 +1,7 @@
+package com.riyaz.spring.springjdbc.employee.dao;
+
+import com.riyaz.spring.springjdbc.employee.dto.Employee;
+
+public interface EmployeeDAO {
+	int create(Employee e);
+}
---
+++ 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/employee/dao/impl/EmployeeDAOImpl.java
@@ -0,0 +1,13 @@
+package com.riyaz.spring.springjdbc.employee.dao.impl;
+
+import com.riyaz.spring.springjdbc.employee.dao.EmployeeDAO;
+import com.riyaz.spring.springjdbc.employee.dto.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Override
+	public int create(Employee e) {
+		return 0;
+	}
+
+}
