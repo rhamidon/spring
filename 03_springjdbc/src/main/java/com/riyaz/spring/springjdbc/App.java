fa076794bc777143a4313707ce459514a375af7a
rhamidon <dcsd143045@student.nibm.lk>
2018-11-29 11:14:39 +0530

    done jdbc insert using Template

--- 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/App.java
+++ 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/App.java
@@ -2,6 +2,7 @@ package com.riyaz.spring.springjdbc;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Hello world!
@@ -9,8 +10,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 	public static void main(String[] args) {
-		AbstractApplicationContext ctx = 
-				new ClassPathXmlApplicationContext("com/riyaz/spring/springjdbc/config.xml");
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("com/riyaz/spring/springjdbc/config.xml");
+		JdbcTemplate tmp = ctx.getBean("jdbcTemplate", JdbcTemplate.class);
+
+		String sql = "insert into employee values(?,?,?)";
+		
+		int result = tmp.update(sql, 1, "Riyaz", "Hamidon");
+		
+		System.out.println("no of records inserted: " + result);
 		ctx.close();
 	}
 }
