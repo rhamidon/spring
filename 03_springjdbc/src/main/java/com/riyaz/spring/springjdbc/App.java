9dc714f64760e2e612b65d4701ead1e00caa7f95
rhamidon <dcsd143045@student.nibm.lk>
2018-11-29 10:52:39 +0530

    add DriverManagerDataSource support

--- 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/App.java
+++ 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/App.java
@@ -1,11 +1,16 @@
 package com.riyaz.spring.springjdbc;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		AbstractApplicationContext ctx = 
+				new ClassPathXmlApplicationContext("com/riyaz/spring/springjdbc/config.xml");
+		ctx.close();
 	}
 }
