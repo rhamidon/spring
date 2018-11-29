b2b45643dfb4092b8338891af0e3e70c30ea3c8f
rhamidon <dcsd143045@student.nibm.lk>
2018-11-29 11:24:24 +0530

    implemented employeedao create method

--- 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/employee/dao/impl/EmployeeDAOImpl.java
+++ 03_springjdbc/src/main/java/com/riyaz/spring/springjdbc/employee/dao/impl/EmployeeDAOImpl.java
@@ -1,13 +1,26 @@
 package com.riyaz.spring.springjdbc.employee.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.riyaz.spring.springjdbc.employee.dao.EmployeeDAO;
 import com.riyaz.spring.springjdbc.employee.dto.Employee;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
 
+	private JdbcTemplate jdbcTemplate;
+
 	@Override
 	public int create(Employee e) {
-		return 0;
+		String sql = "insert into employee values(?,?,?)";
+		return jdbcTemplate.update(sql, e.getId(), e.getFirstName(), e.getLastName());
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 }
