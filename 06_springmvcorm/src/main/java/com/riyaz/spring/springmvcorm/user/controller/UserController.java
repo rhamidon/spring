cfcd18157b675b71fbfd5225089571433468dc79
rhamidon <dcsd143045@student.nibm.lk>
2018-11-30 18:38:13 +0530

    added Controller methods and user-register.jsp page

--- 06_springmvcorm/src/main/java/com/riyaz/spring/springmvcorm/user/controller/UserController.java
+++ 06_springmvcorm/src/main/java/com/riyaz/spring/springmvcorm/user/controller/UserController.java
@@ -2,8 +2,12 @@ package com.riyaz.spring.springmvcorm.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.riyaz.spring.springmvcorm.user.entity.User;
 import com.riyaz.spring.springmvcorm.user.services.UserService;
 
 @Controller
@@ -19,5 +23,17 @@ public class UserController {
 
 	public void setService(UserService service) {
 		this.service = service;
+	}
+	
+	@RequestMapping("/register")
+	public String showForm() {
+		return "user-register";
+	}
+	
+	@RequestMapping(value="/processRegister", method=RequestMethod.POST)
+	public String processForm(@ModelAttribute("user") User user, ModelMap model) {
+		int result = service.save(user);
+		model.addAttribute("result", "user created with id: " + result);
+		return "user-register";
 	}
 }
