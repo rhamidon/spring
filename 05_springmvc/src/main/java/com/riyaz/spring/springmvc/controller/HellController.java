dbe2ebd2d53ab03c014afcc7216a426fcc028e07
rhamidon <dcsd143045@student.nibm.lk>
2018-11-30 14:35:47 +0530

    added support for servlet 3.0 in web.xml

--- 05_springmvc/src/main/java/com/riyaz/spring/springmvc/controller/HellController.java
+++ 05_springmvc/src/main/java/com/riyaz/spring/springmvc/controller/HellController.java
@@ -1,6 +1,7 @@
 package com.riyaz.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,6 +10,9 @@ public class HellController {
 	
 	@RequestMapping("/hello")
 	public ModelAndView hello() {
-		return new ModelAndView("hello");
+		ModelAndView mv = new ModelAndView("hello");
+		mv.addObject("course", "Spring Framework in Easy Steps");
+		mv.addObject("creator", "Bharath Thippreddy");
+		return mv;
 	}
 }
